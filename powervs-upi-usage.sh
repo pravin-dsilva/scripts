512a14591edd1aa64d884fcaa77333d767e62094
Pravin D Silva <pravin.dsilva@ibm.com>
2023-01-23 20:03:26 +0530

    Update powervs-upi-usage.sh

--- powervs-upi-usage.sh
+++ powervs-upi-usage.sh
@@ -11,3 +11,15 @@ VMS=`ibmcloud pi ins |awk "(NR>1)" |  awk '{print $2}' | wc -l`
 echo "****Number of VM's: $VMS ****"
 ibmcloud pi ins  |awk "(NR>1)" |  awk '{print $2}'
 done
+echo "IPI USAGE:"
+for i in rdr-ocp-ipi-validation-lon06
+do
+echo "==========================================================================="
+echo "=====================Region: ${i}========================"
+echo "==========================================================================="
+CRN=`ibmcloud pi sl | grep  $i | cut -d " " -f1`
+ibmcloud pi st $CRN
+VMS=`ibmcloud pi ins |awk "(NR>1)" |  awk '{print $2}' | wc -l`
+echo "****Number of VM's: $VMS ****"
+ibmcloud pi ins  |awk "(NR>1)" |  awk '{print $2}'
+done
